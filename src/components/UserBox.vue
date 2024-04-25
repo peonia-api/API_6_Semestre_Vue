1cb442d60bfbbd32c44fbd05c847c61b870cb271
Wallace Marinho <wallace.marinhosouzas@gmail.com>
2024-04-25 12:13:40 -0300

    configurated CSS from UserBox Component

--- src/components/UserBox.vue
+++ src/components/UserBox.vue
@@ -1,13 +1,31 @@
 <template>
    <div class="container-box">
-     <div class="box">
+     <div :class="[
+      'box',
+      {
+          blue_color: background_color == 'blue_color',
+          gray_color: background_color == 'gray_color'
+      }
+     ]">
         <slot></slot>
      </div>
    </div>
 </template>
 
 
-<script setup>
+<script lang="ts">
+import type { PropType } from 'vue';
+
+export default {
+  props: {
+    background_color: {
+      type: String as PropType<"blue_color" | "gray_color">,
+      default: "blue_color"
+    }
+
+  }
+    
+}
 </script>
 
 
@@ -19,10 +37,18 @@
     min-height: calc(100vh - 150px);
   }
 
+.blue_color {
+  background-color: rgba(0, 51, 101, 1);
+}
+
+.gray_color {
+  background-color: red;
+}
+
+
   .box {
     width: 650px;
     height: 440px;
-    background-color: rgba(0, 51, 101, 1);
     display: flex;
     flex-direction: column; 
     align-items: center; 
