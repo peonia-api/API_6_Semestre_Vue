e1b7090bf6c45b6d88af34436ac14fbea563b5fc
Wallace Marinho <wallace.marinhosouzas@gmail.com>
2024-05-10 21:02:11 -0300

    Feat: Added DropDowns for 'Área' and 'Guardas'

--- src/views/RedzoneRegistration.vue
+++ src/views/RedzoneRegistration.vue
@@ -5,29 +5,36 @@
            <InputText class="input-field" id="nomeredzone" v-model="value" />
            <label for="nomeredzone">Nome</label>
         </FloatLabel>
+
         <FloatLabel class="field">
-           <InputText class="input-field" id="arearedzone" v-model="value" />
-           <label for="arearedzone">Área</label>
+           <Dropdown class="DropDown-style" v-model="selectedArea" inputId="id-area" :options="areas" optionLabel="name"/>
+           <label for="id-area">Área</label>
         </FloatLabel>
+
         <FloatLabel class="field">
            <InputText class="input-field" id="responsaveldzone" v-model="value" />
            <label for="responsavelredzone">Responsável</label>
         </FloatLabel>
+
         <FloatLabel class="field">
            <InputText class="input-field" id="capmaximaredzone" v-model="value" />
            <label for="capmaximaredzone">Capacidade Máxima</label>
         </FloatLabel>
+
         <FloatLabel class="field">
-           <InputText class="input-field" id="guardasredzone" v-model="value" />
-           <label for="guardasredzone">Guardas</label>
+           <Dropdown class="DropDown-style" v-model="selectedGuard" inputId="id-guards" :options="guards" optionLabel="name"/>
+           <label for="dd-city">Guardas</label>
+         </FloatLabel>
+
+         <FloatLabel class="field">
+           <InputText class="input-field" id="funcaoredzone" v-model="value" />
+           <label for="funcaoredzone">Função</label>
         </FloatLabel>
-      <div class="input-container">
-        <InputText class="input-field" type="text" placeholder="Função" />
-      </div>
+
       <div class="Register-Button">
       <Button label="Cadastrar" severity="contrast"></Button>
     </div>
-    </div>
+  </div>
   </UserBox>
  
 
@@ -39,6 +46,24 @@ import UserBox from '@/components/UserBox.vue';
 import InputText from 'primevue/inputtext';
 import FloatLabel from 'primevue/floatlabel';
 import Button from 'primevue/button';
+import Dropdown from 'primevue/dropdown';
+
+import { ref } from "vue";
+
+const selectedGuard = ref();
+const guards = ref([
+    { name: 'Guarda 1', code: 'G1' },
+    { name: 'Guarda 2', code: 'G2' },
+    { name: 'Guarda 3', code: 'G3' },
+]);
+
+const selectedArea = ref();
+const areas = ref([
+    { name: 'Área 1', code: 'A1' },
+    { name: 'Área 2', code: 'A2' },
+    { name: 'Área 3', code: 'A3' },
+]);
+
 
 
 
@@ -81,13 +106,17 @@ import Button from 'primevue/button';
     width: auto;
   }
 
+  .DropDown-style {
+     width: 430px;
+  }
+
 .FloatLabel label {
   position: absolute;
   top: 0;
   left: 0;
   transition: transform 0.3s ease;
   transform-origin: 0 0;
-  color: #333; /* Cor do rótulo */
+  color: #333; 
 }
 
 .FloatLabel input:focus + label,
