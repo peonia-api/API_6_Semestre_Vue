6d79bb8012cd5bb8400f2163590e26a109564a16
Diego Silva <diegosilva157890@gmail.com>
2024-05-21 19:00:16 -0300

    Feat: add function to edit area

--- src/views/AreaEdit.vue
+++ src/views/AreaEdit.vue
@@ -5,22 +5,18 @@
     <UserBox background_color="blue_color">
         <div class="Input-Texts">
             <div class="input-container">
-                <InputText type="text" placeholder="Nome" />
+                <InputText type="text" v-model="areaData.name" placeholder="Nome" />
             </div>
             <div class="input-container">
-                <InputText type="text" placeholder="Descrição" />
+                <InputText type="text" v-model="areaData.description" placeholder="Descrição" />
             </div>
-            <div>
-                <select class="input-select">
-                    <option selected disabled>Responsável</option>
-                    <option value="testteet">teste</option>
-                    <option value="testteet">teste2</option>
-                </select>
+            <div class="input-container">
+                <MultiSelect v-model="selectedRedZones" :options="areaData.redZones" optionLabel="name" placeholder="Select Red Zones" class="w-full md:w-20rem" />
             </div>
         </div>
         <div class="Register-Button">
-            <Button label="Voltar" class="button-edit" severity="contrast"></Button>
-            <Button label="Editar" class="button-edit" severity="contrast"></Button>
+            <Button label="Voltar" class="button-edit" severity="contrast" @click="submitVoltar"></Button>
+            <Button label="Editar" class="button-edit" severity="contrast" @click="submitPutForm"></Button>
         </div>
     </UserBox>
 </template>
@@ -28,9 +24,67 @@
 <script setup>
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
+import MultiSelect from 'primevue/multiselect';
 import UserBox from '@/components/UserBox.vue';
+import AreaStore from '../stores/Area';
+import { useRouter, useRoute } from 'vue-router';
+import { onMounted, ref } from 'vue';
+import { avisoEditar, avisoVoltar } from '../plugins/sweetalert';
 
+const router = useRouter();
+const { findByIdArea, putArea } = AreaStore();
+const route = useRoute();
+
+const areaId = Array.isArray(route.params.id) ? route.params.id[0] : route.params.id;
+
+const areaData = ref({
+  name: '',
+  description: '',
+  responsibleManager: 'Gerente Responsável',
+  user: {
+    id: ''
+  },
+  redZones: [] 
+});
+
+const selectedRedZones = ref([]);
+
+async function getArea() {
+  try {
+    const response = await findByIdArea(areaId);
+    areaData.value = { ...response }; 
+    selectedRedZones.value = areaData.value.redZones.map(redZone => redZone.id);
+  } catch (error) {
+    console.log('Erro ao buscar informações do usuário:', error);
+  }
+}
+onMounted(async () => {
+  await getArea();
+});
+
+async function submitPutForm() {
+  const result = await avisoEditar();
+  if (result.isConfirmed) {
+    try {
+      const updatedRedZones = areaData.value.redZones.filter(redZone => selectedRedZones.value.includes(redZone.id));
+      areaData.value.redZones = updatedRedZones;
+        console.log(areaData.value);
+      await putArea(areaId, areaData.value);
+      router.push("/areaList");
+    } catch (error) {
+      console.error('Erro ao editar área:', error);
+    }
+  }
+}
+
+async function submitVoltar() {
+  const result = await avisoVoltar();
+  if (result.isConfirmed) {
+    router.push("/areaList");
+  }
+}
 </script>
+
 
 
 <style scoped>
