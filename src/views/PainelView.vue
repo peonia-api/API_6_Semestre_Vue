3c1ef73f40687fd3dea01f05007e0e5ac5e85f75
Diego Silva <diegosilva157890@gmail.com>
2024-05-25 19:49:28 -0300

    Fix: The names rooms

--- src/views/PainelView.vue
+++ src/views/PainelView.vue
@@ -1,48 +1,50 @@
 <template>
-    <div class="title-ocorrencias">
-      <p>Ocorrências {{ redzoneName }}</p>
+  <div class="title-ocorrencias">
+    <p>Ocorrências {{ redzoneName }}</p>
+  </div>
+  <div class="painel-container">
+    <div class="export-dropdown">
+      <img src="@/assets/icons/Export_Icon.png" alt="Exportar Tabela para Excel" @click="exportToExcel" class="export-icon">
     </div>
-    <div class="painel-container">
-      <div class="export-dropdown">
-        <img src="@/assets/icons/Export_Icon.png" alt="Exportar Tabela para Excel" @click="exportToExcel" class="export-icon">
-      </div>
-      <div>
-        <Grafico></Grafico>
-      </div>
-      <div class="table-container">
-        <TableReports :items-per-page="4"></TableReports>
-      </div>
+    <div>
+      <Grafico></Grafico>
     </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { defineProps } from 'vue';
-  import Grafico from '../components/Grafico.vue';
-  import TableReports from '../components/TableReports.vue';
-  import * as XLSX from 'xlsx';
-  
- defineProps<{ redzoneName: string }>();
-  
-  const exportToExcel = () => {
-    const formattedData = JSON.parse(localStorage.getItem('formattedData') || '');
-  
-    if (formattedData) {
-      const worksheet = XLSX.utils.json_to_sheet(formattedData.map((item: { occurrence: string; formattedDate: any; formattedTime: any; }):any => ({
-        'Ocorrência': item.occurrence === '0' ? 'saída' : 'entrada',
-        'Data': item.formattedDate,
-        'Hora': item.formattedTime,
-        'Sala': 'Laboratório'
-      })));
-  
-      const workbook = XLSX.utils.book_new();
-      XLSX.utils.book_append_sheet(workbook, worksheet, 'Ocorrências');
-      XLSX.writeFile(workbook, 'relatorio_ocorrencias.xlsx');
-    } else {
-      console.log('Não há dados formatados no localStorage.');
-    }
-  };
-  </script>
-  
+    <div class="table-container">
+      <TableReports :items-per-page="4" :redzoneName="redzoneName"></TableReports>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import Grafico from '../components/Grafico.vue';
+import TableReports from '../components/TableReports.vue';
+import * as XLSX from 'xlsx';
+
+const props = defineProps<{ redzoneName: string }>();
+
+console.log(props.redzoneName);
+const exportToExcel = () => {
+  const formattedData = JSON.parse(localStorage.getItem('formattedData') || '');
+
+  if (formattedData) {
+    const worksheet = XLSX.utils.json_to_sheet(formattedData.map((item: { occurrence: string; formattedDate: any; formattedTime: any; }): any => ({
+      'Ocorrência': item.occurrence === '0' ? 'saída' : 'entrada',
+      'Data': item.formattedDate,
+      'Hora': item.formattedTime,
+      'Sala': 'Laboratório'
+    })));
+
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Ocorrências');
+    XLSX.writeFile(workbook, 'relatorio_ocorrencias.xlsx');
+  } else {
+    console.log('Não há dados formatados no localStorage.');
+  }
+};
+</script>
+
+
   <style>
   .painel-container {
       display: flex;
